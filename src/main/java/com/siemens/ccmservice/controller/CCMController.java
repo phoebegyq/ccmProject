a452a13b4a90b1bf28037a724e6174f0dcfcad7e
Yuqin Ge <yuqin.ge@siemens.com>
2017-12-22 11:08:01 +0800

    date

--- src/main/java/com/siemens/ccmservice/controller/CCMController.java
+++ src/main/java/com/siemens/ccmservice/controller/CCMController.java
@@ -11,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
+import java.time.YearMonth;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/rest")
@@ -21,17 +25,37 @@ public class CCMController {
     private QueryDateService queryDateService;
 
     @RequestMapping("/ppm")
-    public List<String> getPPMInfo(PPMBeanRequest req) throws ParseException {
+    public String getPPMInfo(PPMBeanRequest req) throws ParseException {
         String json = HttpClientUtil.doPostJson("http://localhost:8300/rest/ppm", JsonUtils.objectToJson(req));
 
-//        List<VPPMBean> list = JsonUtils.jsonToList(json, VPPMBean.class);
-
-        List<String> month = queryDateService.getMonthBetween(req.getStartDate().toString(), req.getEndDate().toString());
+        List<VPPMBean> list = JsonUtils.jsonToList(json, VPPMBean.class);
 
 
-        return month;
+//        @RequestMapping(path = "/ccm", method = RequestMethod.POST)
+//        public String getCCMInfo(@RequestBody CCMBeanRequest req) throws Exception {
+//            List<CCMBean> list = ccmMapper.findAll();
+            Map<YearMonth, Integer> monthList = new HashMap<>();
+            for (VPPMBean tmpobj : list) {
+                Calendar cal=Calendar.getInstance();
+                cal.setTime(tmpobj.getComplaintCreateDate());
+                Boolean hasMonth = false;
+                for (YearMonth tmpMonth : monthList.keySet()) {
+
+                    if(cal.get(Calendar.YEAR) == tmpMonth.getYear() && cal.get(Calendar.MONTH)+1 == tmpMonth.getMonthValue()){
+                        int complaint = monthList.get(tmpMonth);
+                        complaint += Integer.parseInt(tmpobj.getComplaintQuantity());
+                        monthList.put(tmpMonth, complaint);
+                        hasMonth = true;
+                        break;
+                    }
+                }
+                if(hasMonth == false){
+                    monthList.put(YearMonth.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1), Integer.parseInt(tmpobj.getComplaintQuantity()));
+                }
+            }
+
+            return "ok";
 
     }
-
 
 }
